bb1c6287b09878b0197e931545fdae9edf055ae6
Mike <137076652+miketheebike@users.noreply.github.com>
2024-11-20 01:27:22 +0000

    Update changing_components.py

--- changing_components.py
+++ changing_components.py
@@ -44,15 +44,13 @@ def add_submission():
     data[USER_FULL_NAME].append(safe_var('user_full_name'))
     data[USER_POSITION].append(safe_var('user_position'))
     data[USER_PROF_CATEGORY].append(safe_var('professional_category'))
-    st.session_state['data'] = data
-    for key, value in data.items():
-        st.write(f"{key}: Length = {len(value)}")
+
     session_state_df = pd.DataFrame(data)
     
-    personal_data_df = session_state_df.iloc[:, :5]
-    min_eff_df = session_state_df.iloc[:, 5:]
+    #personal_data_df = session_state_df.iloc[:, :5]
+    #min_eff_df = session_state_df.iloc[:, 5:]
 
-    concatenated_df = pd.concat([personal_data_df, questions_df.set_index(personal_data_df.index), min_eff_df.set_index(personal_data_df.index)], axis=1)
+    #concatenated_df = pd.concat([personal_data_df, questions_df.set_index(personal_data_df.index), min_eff_df.set_index(personal_data_df.index)], axis=1)
       
     st.session_state['submit'] = True
     
@@ -62,7 +60,7 @@ def add_submission():
     creds = ServiceAccountCredentials.from_json_keyfile_dict(secrets_to_json(), scope)
     client = gspread.authorize(creds)
  
-    sheet = client.open("EEnergy_Efficiency_Survey_Data").sheet1
+    sheet = client.open("EEN_Survey_Data").sheet1
 
     column_names_list = concatenated_df.columns.tolist()
     #column_names = sheet.append_row(column_names_list)
