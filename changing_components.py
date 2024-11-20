1d0ae0f7d854fa4af4525f5719dbd239f83fcc5a
Mike <137076652+miketheebike@users.noreply.github.com>
2024-11-20 21:20:17 +0000

    Update changing_components.py

--- changing_components.py
+++ changing_components.py
@@ -217,6 +217,7 @@ def create_question(jsonfile_name):
             height = 400
             )
             st.plotly_chart(fig, config = config ,use_container_width=True)
+            updated_bins_list = pd.DataFrame(bins_grid)
             def restructure_df(df, i):
                 transposed_df = df.transpose()
                 transposed_df.columns =  [f'Q{i + 1}  {col}' for col in list(transposed_df.iloc[0])]
