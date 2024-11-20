68d71aefe8133118e36e071178a8c1084e1e06d4
Mike <137076652+miketheebike@users.noreply.github.com>
2024-11-20 04:30:00 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ col1, _= st.columns(2)
 with col1:   
     st.slider("Please move the slider to indicate your preference.", 1, 10, key= "risk_aversion")
 
-submit = st.button("Submit", on_click = add_submission)
+submit = st.button("Submit", on_click = add_submission(question_variables))
 
 if st.session_state['submit']:
     st.success(f"Thank you for completing the Survey on {config['header']['survey_title']}!")
