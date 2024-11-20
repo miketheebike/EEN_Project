3a493cc9a4d9c3a4ea5c10f3ca06dcb820527dc5
Mike <137076652+miketheebike@users.noreply.github.com>
2024-11-20 00:29:05 +0000

    Update fixed_components.py

--- fixed_components.py
+++ fixed_components.py
@@ -14,4 +14,20 @@ import csv
 import altair as alt
 import plotly.graph_objs as go
 from streamlit_sortables import sort_items
+
+def secrets_to_json():
+    return {
+        "folder_id": st.secrets["folder_id"],
+        "type": st.secrets["type"],
+        "project_id": st.secrets["project_id"],
+        "private_key_id": st.secrets["private_key_id"],
+        "private_key": st.secrets["private_key"],
+        "client_email": st.secrets["client_email"],
+        "client_id": st.secrets["client_id"],
+        "auth_uri": st.secrets["auth_uri"],
+        "token_uri": st.secrets["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+        "universe_domain": st.secrets["universe_domain"],
         
+    }
