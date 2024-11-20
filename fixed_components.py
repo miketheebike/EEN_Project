5290577ed03c15dcabd11c9bbc1879a78f7c0a68
Mike <137076652+miketheebike@users.noreply.github.com>
2024-11-20 00:24:38 +0000

    Update fixed_components.py

--- fixed_components.py
+++ fixed_components.py
@@ -1,1 +1,17 @@
-
+import streamlit as st
+import streamlit.components.v1 as components
+from datetime import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+from google.oauth2 import service_account
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import io
+import numpy as np
+import requests
+#from requests_oauthlib import OAuth2Session
+import csv
+import altair as alt
+import plotly.graph_objs as go
+from streamlit_sortables import sort_items
+        
