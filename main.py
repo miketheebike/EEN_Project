61b3487709f0ad0a80db30e73a16e22fef9858cb
Mike <137076652+miketheebike@users.noreply.github.com>
2024-11-20 00:31:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ import json
 from fixed_components import *
 from changing_components import *
 import numpy as np
+
+st.set_page_config(layout="wide")
+
+# Read the JSON file
+config = json.load(open('config.json'))
+
+initialize_session_state()
+personal_information()
+submit = st.button("Submit", on_click = add_submission)
+
+if st.session_state['submit']:
+    st.success(f"Thank you for completing the Survey on {config['header']['survey_title']}!")
