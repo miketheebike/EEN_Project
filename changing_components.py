5417e3422a1843f62f3e99bf01d9ea6cdeba6a39
Mike <137076652+miketheebike@users.noreply.github.com>
2024-11-21 10:15:56 +0000

    Update changing_components.py

--- changing_components.py
+++ changing_components.py
@@ -59,6 +59,7 @@ def initialize_session_state():
             'User Professional Category': [],
             'Years as Advisor': [],
             'Join Date EEN': [],
+            'Expert or Generalist': [],
             'Minimum Effect Size Q1': [],
             'Minimum Effect Size Q2': [],    
             'Minimum Effect Size Q3': [],
