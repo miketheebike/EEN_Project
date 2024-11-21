9afa1efc61e400df7d56c15da07c64572edb7da3
Mike <137076652+miketheebike@users.noreply.github.com>
2024-11-21 09:55:48 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,8 +59,8 @@ if st.session_state['consent'] == True:
     effect_size_question6 = effect_size_question(q6_config)
         
     
-    q7_config = config['question7']
-    updated_bins_question_7_df, percentage_difference7, num_bins7 = create_question(q7_config)
+    qX_config = config['questionX']
+    updated_bins_question_X_df, percentage_differenceX, num_binsX = create_question(qX_config)
     #effect_size_question9 = effect_size_question(q9_config)
     
     col2, _ = st.columns(2)
@@ -68,14 +68,14 @@ if st.session_state['consent'] == True:
         st.image("SatSunGraph.png", width = 350)
     st.write("Saturday and Sunday temperatures in Washington DC for each weekend in 2022. As we might expect, there is a strong correlation between the temperature on a Saturday and on the Sunday, since some parts of the year are hot, and others colder. The correlation here is 0.88.")
     
-    q8_config = config['question8']
-    updated_bins_question_8_df, percentage_difference8, num_bins8 = create_question(q8_config)    
+    q7_config = config['question7']
+    updated_bins_question_7_df, percentage_difference8, num_bins7 = create_question(q7_config)    
     
     percentage_differences = [percentage_difference1, percentage_difference2] #, percentage_difference3, percentage_difference4, percentage_difference5
     updated_bins_list = [updated_bins_question_1_df, updated_bins_question_2_df]#, updated_bins_question_3_df, updated_bins_question_4_df, updated_bins_question_5_df
     
     
-    st.subheader("Question 9 - Cost/Benefit Ratio")
+    st.subheader("Question 8 - Cost/Benefit Ratio")
     st.write("In simple terms, a cost-benefit ratio is used to compare the costs of a project against the benefits it delivers. For instance, if a program costs €100.000 and the monetized value of its benefits is €150.000, the cost-benefit ratio would be 1:1.5. This means that for every euro spent, the program delivers one and a half euro in benefits. A higher ratio indicates greater efficiency and value for money. This question prompts to consider the efficiency and economic justification for scaling a program, ensuring that the decision aligns with both fiscal responsibility and the desired impact. At what cost-benefit ratio would you consider scaling the EEnergy Efficiency Project?\n Consider “benefits” that occurred after two years of running the program and “costs” as the total expenses incurred to implement, operate, and maintain a program or project (including administration and overhead costs).")
     
     col1, _= st.columns(2)
@@ -84,7 +84,7 @@ if st.session_state['consent'] == True:
         cost_benefit_list = [f"1:{round(i, 1)}" for i in np.arange(0.6, 3.1, .2)]
         st.select_slider("Please move the slider to indicate your preference.", cost_benefit_list, key = "cost_benefit")
     
-    st.subheader("Question 10 - Risk Aversion")
+    st.subheader("Question 9 - Risk Aversion")
     st.write("Rate your willingness to take risks in general on a 10-point scale, with 1 completely unwilling and 10 completely willing.")
     
     col1, _= st.columns(2)
@@ -132,7 +132,7 @@ if st.session_state['consent'] == True:
     # with col1:   
     #     st.slider("Please move the slider to indicate your preference.", 1, 10, key= "risk_aversion")
     
-    submit = st.button("Submit", on_click= add_submission, args = ([updated_bins_question_1_df, updated_bins_question_2_df, updated_bins_question_3_df, updated_bins_question_4_df, updated_bins_question_5_df, updated_bins_question_6_df, updated_bins_question_7_df, updated_bins_question_8_df]))
+    submit = st.button("Submit", on_click= add_submission, args = ([updated_bins_question_1_df, updated_bins_question_2_df, updated_bins_question_3_df, updated_bins_question_4_df, updated_bins_question_5_df, updated_bins_question_6_df, updated_bins_question_7_df]))
     
     #
     if st.session_state['submit']:
